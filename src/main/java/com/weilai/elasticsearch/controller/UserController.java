3e8d4624c390ee77ebd4c7d6ea513ffdec77c595
miku-wwl <569144003@qq.com>
2024-11-01 00:51:24 +0800

    feat:userRepository.searchSimilar(user, new String[]{"name"}, PageRequest.of(0,20));

--- src/main/java/com/weilai/elasticsearch/controller/UserController.java
+++ src/main/java/com/weilai/elasticsearch/controller/UserController.java
@@ -3,13 +3,15 @@ package com.weilai.elasticsearch.controller;
 import com.weilai.elasticsearch.entity.User;
 import com.weilai.elasticsearch.repository.UserRepository;
 import com.weilai.elasticsearch.service.UserService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -49,4 +51,70 @@ public class UserController {
     public List<User> searchByNameLike(@RequestParam String name) {
         return userRepository.findByNameLike(name);
     }
+
+
+    /**
+     * create these records and only when id =7、9, it returns records.
+     * haven't understood the principle behind the similarity judgment yet.
+     * [
+     *   {
+     *     "id": "2",
+     *     "name": "w2",
+     *     "email": "w2"
+     *   },
+     *   {
+     *     "id": "3",
+     *     "name": "w2",
+     *     "email": "w2"
+     *   },
+     *   {
+     *     "id": "4",
+     *     "name": "w2",
+     *     "email": "w2"
+     *   },
+     *   {
+     *     "id": "1",
+     *     "name": "w1",
+     *     "email": "w1"
+     *   },
+     *   {
+     *     "id": "5",
+     *     "name": "w1 w2 w3 w4 s1 s2",
+     *     "email": "w1"
+     *   },
+     *   {
+     *     "id": "6",
+     *     "name": "w1 w2 w3 w4 s1 s2",
+     *     "email": "w1"
+     *   },
+     *   {
+     *     "id": "7",
+     *     "name": "s1 s2 w2 w2",
+     *     "email": "w1"
+     *   },
+     *   {
+     *     "id": "8",
+     *     "name": "s1 s2 w2 w3",
+     *     "email": "w1"
+     *   },
+     *   {
+     *     "id": "9",
+     *     "name": "s1 s2 w2 s2",
+     *     "email": "w1"
+     *   }
+     * ]
+     *
+     *
+     */
+    @GetMapping("/searchSimilarByName")
+    public Page<User> searchSimilarByName(@RequestParam String id){
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isEmpty()){
+            return Page.empty();
+        }
+        User user = userOptional.get();
+
+        return userRepository.searchSimilar(user, new String[]{"name"}, PageRequest.of(0,20));
+    }
+
 }
